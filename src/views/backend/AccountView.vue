c7941b8ba67aed7184db817f50b3ee832c528add
carrieTseng <echocarriet@gmail.com>
2024-02-23 13:36:15 +0800

    更新資料出現問題

--- src/views/backend/AccountView.vue
+++ src/views/backend/AccountView.vue
@@ -17,24 +17,18 @@
           刪除帳號
         </button>
       </div>
-      <form class="max-w-3xl mx-auto">
+      <form class="max-w-3xl mx-auto" @submit="updateMember">
         <div class="mb-4">
           <label for="name" class="block mb-2 text-base font-medium text-gray-1">名稱</label>
           <input type="text" id="name" class="bg-white border border-gray-3 text-sm rounded-full
-              focus:ring-primary focus:border-primary block w-full px-3 py-4" v-model="member.name" />
+              focus:ring-primary focus:border-primary block w-full px-3 py-4" v-model="memberPerson.name" />
         </div>
-        <!-- <div class="mb-4">
-          <label for="email" class="block mb-2 text-base font-medium text-gray-1">Email*</label>
-          <input type="email" id="email" class="bg-white border border-gray-3 text-sm rounded-3xl
-              focus:ring-primary focus:border-primary block w-full px-3 py-4" placeholder="xxx@gmail.com" required />
-          <p class="mt-2 text-sm text-gray-2">信箱已驗證成功。若修改信箱，新信箱須重新驗證後才會儲存。</p>
-        </div> -->
         <div class="mb-4">
           <label for="sex" class="block mb-2 text-base font-medium text-gray-1">性別</label>
           <select id="sex"
             class="bg-white border border-gray-300 text-gray-900 text-sm  rounded-full focus:ring-primary focus:border-primary block w-full px-3 py-4"
-            v-model="member.gender">
-            <option value="" selected>請選擇性別</option>
+            v-model="memberPerson.gender">
+            <option value="" selected disabled>請選擇性別</option>
             <option value="male">男</option>
             <option value="female">女</option>
             <option value="x">不方便透露</option>
@@ -45,9 +39,7 @@
           <input type="date" id="birthday" class="bg-white border border-gray-3 text-sm rounded-full
               focus:ring-primary focus:border-primary block w-full px-3 py-4" />
         </div>
-        <hr>
-        <!-- ➀ 社群連結 v-for 渲染出，但data沒得就無法渲染 -->
-        <div class="mb-4">
+        <!-- <div class="mb-4">
           <template v-for="(item, index) in socialMedia" :key="index">
             <div class="mb-4">
               <label for="facebookUrl" class="block mb-2 text-base font-medium text-gray-1">
@@ -57,35 +49,8 @@
               focus:ring-primary focus:border-primary block w-full px-3 py-4 mb-2" placeholder="請填入社群網址"
                 v-model="socialMedia[index].id" />
             </div>
-            <!-- :value="socialMedia[index].type === item.type ? `${socialMedia[index].id}` : '請填入社群網址'" -->
           </template>
-        </div>
-        <!-- ➀ 社群連結 v-for 渲染出，但data沒得就無法渲染 -->
-        <!-- ➁ 社群連結  -->
-        <!-- ➁ 社群連結  -->
-        <hr>
-        <!-- 社群連結 label & input -->
-        <div class="mb-4">
-          <label for="facebookUrl" class="block mb-2 text-base font-medium text-gray-1">Facebook 社群連結</label>
-          <input type="url" id="facebookUrl" class="bg-white border border-gray-3 text-sm rounded-full
-              focus:ring-primary focus:border-primary block w-full px-3 py-4" placeholder="請填入社群網址" />
-        </div>
-        <div class="mb-4">
-          <label for="instagramUrl" class="block mb-2 text-base font-medium text-gray-1">Instagram 社群連結</label>
-          <input type="url" id="instagramUrl" class="bg-white border border-gray-3 text-sm rounded-full
-              focus:ring-primary focus:border-primary block w-full px-3 py-4" placeholder="請填入社群網址" />
-        </div>
-        <div class="mb-4">
-          <label for="youtubeUrl" class="block mb-2 text-base font-medium text-gray-1">YouTube 社群連結</label>
-          <input type="url" id="youtubeUrl" class="bg-white border border-gray-3 text-sm rounded-full
-              focus:ring-primary focus:border-primary block w-full px-3 py-4" placeholder="請填入社群網址" />
-        </div>
-        <div class="mb-6">
-          <label for="personalUrl" class="block mb-2 text-base font-medium text-gray-1">個人網站連結</label>
-          <input type="url" id="personalUrl" class="bg-white border border-gray-3 text-sm rounded-full
-              focus:ring-primary focus:border-primary block w-full px-3 py-4" placeholder="請填入社群網址" />
-        </div>
-        <!-- 社群連結 label & input -->
+        </div> -->
         <button type="submit" class="text-white bg-gray-1 hover:bg-primary focus:ring-4
             focus:outline-none focus:ring-primary-light font-medium rounded-full
             text-base w-full sm:w-auto px-5 py-2.5 text-center">
@@ -94,8 +59,6 @@
       </form>
     </div>
   </div>
-  {{ changesocialMedia }} <br>
-  {{ changesocialMedia2 }}
   <DelModal ref="DelModal" :delContent="delContent"></DelModal>
   <ComponentFooter></ComponentFooter>
 </template>
@@ -117,11 +80,11 @@ export default {
   data() {
     return {
       delContent: '「此帳號後，需重新建立帳號」',
-      socialMedia2: [],
+      memberId: '',
       socialMedia: [
         {
           type: 'Facebook',
-          id: 'johndoe',
+          id: '',
         },
         {
           type: 'Instagram',
@@ -132,11 +95,11 @@ export default {
           id: '',
         },
         {
-          type: 'PersonalUrl',
+          type: '個人網站',
           id: '',
         },
       ],
-      member: {
+      memberPerson: { // 根據ID獲取會員詳細資料
         follwing: [],
         followers: [],
         socialMedia: [],
@@ -163,8 +126,8 @@ export default {
           if (response.data.status) {
             console.log(response);
             const { _id } = response.data.result;
-            const memberId = _id;
-            this.getMemberInfo(memberId);
+            this.memberId = _id;
+            this.getMemberInfo(this.memberId);
           } else {
             this.$swal({
               title: '登入失敗，請重新登入',
@@ -182,8 +145,8 @@ export default {
       const apiUrl = `${import.meta.env.VITE_APP_API_URL}/api/member/${id}`;
       this.$http.get(apiUrl)
         .then((res) => {
-          this.member = res.data.result;
-          console.log(this.member);
+          this.memberPerson = res.data.result;
+          console.log(this.memberPerson);
           // this.$swal({
           //   title: `${res.data.message}`,
           // });
@@ -195,28 +158,23 @@ export default {
           });
         });
     },
-  },
-  computed: {
-    changesocialMedia() {
-      return this.socialMedia;
+    updateMember() {
+      const apiUrl = `${import.meta.env.VITE_APP_API_URL}/api/member/`;
+      this.$http.put(apiUrl)
+        .then((res) => {
+          console.log(res);
+          this.getMemberInfo(this.memberId);
+          this.$swal({
+            title: `${res.data.message}`,
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          this.$swal({
+            title: `${err.response.data.message}`,
+          });
+        });
     },
-    changesocialMedia2() {
-      return this.socialMedia2;
-    },
-  },
-  watch: {
-    socialMedia: {
-      // 控制器(參數n為新值,參數o為舊值)
-      handler(n) {
-        // socialMedia有變動就push到遠端資料庫
-        this.socialMedia2.push(n);
-      },
-      // 會去定義它是否為深層監聽,此為固定結構
-      deep: true,
-    },
-    // socialMedia(n) {
-    //   console.log(n);
-    // },
   },
   mounted() {
     this.getToken();
